9e4ac0895554d1631e9b8982b112e7e274b535cf
Mozul504 <mozul.ai@phystech.edu>
2026-09-08 21:41:56 +0300

    Решение задачи 3

---
+++ seminar1_hello/03.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d", &a);
+
+    if ((a % 2 == 0) && ((a >= 0 && a <= 20) || (a > 100 && a < 200)))
+        printf("Yes\n");
+    else
+        printf("No\n");
+
+    return 0;
+}
