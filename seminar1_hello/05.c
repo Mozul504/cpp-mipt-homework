64044e914dc2e28694dc67b2c3c6bb22f84dc3ca
Александр Мозуль <mozul.ai@phystech.edu>
2026-09-23 10:55:28 +0300

    Update 05.c

--- seminar1_hello/05.c
+++ seminar1_hello/05.c
@@ -6,22 +6,25 @@ int main(void)
     int n = 0;
     int max = INT_MIN;
     int max_count = 0;
+    int x;
 
     scanf("%d", &n);
 
     for (int i = 0; i < n; i++)
     {
-        scanf("%d", &i);
+        scanf("%d", &x);
 
-        if (i > max)
+        if (x > max)
         {
-            max = i;
+            max = x;
             max_count = 1;
         }
-
-        else if (i == max)
+        else if (x == max)
+        {
             max_count++;
+        }
     }
+
     printf("MAX = %d\n", max);
     printf("MAX_COUNT = %d\n", max_count);
 
